040a6eba17eedb302e44444f8e5e62112338a644
vandanaranasara <vandanaranasara03102004@gmail.com>
2025-10-29 15:26:21 +0530

    signup page updated

--- store/views/signup.py
+++ store/views/signup.py
@@ -5,69 +5,74 @@ from django.views import View
 from store.tasks import send_welcome_email_task
 
 
-class Signup (View):
+class Signup(View):
     def get(self, request):
-        return render (request, 'signup.html')
+        return render(request, 'signup.html')
 
     def post(self, request):
         postData = request.POST
-        first_name = postData.get ('firstname')
-        last_name = postData.get ('lastname')
-        phone = postData.get ('phone')
-        email = postData.get ('email')
-        password = postData.get ('password')
-        
-        # validation
+        first_name = postData.get('firstname')
+        last_name = postData.get('lastname')
+        phone = postData.get('phone')
+        email = postData.get('email')
+        password = postData.get('password')
+        confirm_password = postData.get('confirm_password')  # ✅ new line
+
+        # validation values for re-render
         value = {
             'first_name': first_name,
             'last_name': last_name,
             'phone': phone,
             'email': email
         }
+
         error_message = None
-        
-        customer = Customer (first_name=first_name,
-                            last_name=last_name,
-                            phone=phone,
-                            email=email,
-                            password=password)
-        error_message = self.validateCustomer (customer)
+
+        if password != confirm_password:
+            error_message = "Passwords do not match!"
+        else:
+            customer = Customer(
+                first_name=first_name,
+                last_name=last_name,
+                phone=phone,
+                email=email,
+                password=password
+            )
+            error_message = self.validateCustomer(customer)
 
         if not error_message:
-            print (first_name, last_name, phone, email, password)
-            customer.password = make_password (customer.password)
-            customer.register ()
-            
+            customer.password = make_password(customer.password)
+            customer.register()
+
+            # Send welcome email asynchronously
             send_welcome_email_task.delay(email)
-            
-            return redirect ('store')
+
+            return redirect('store')
         else:
             data = {
                 'error': error_message,
                 'values': value
             }
-            return render (request, 'signup.html', data)
+            return render(request, 'signup.html', data)
 
     def validateCustomer(self, customer):
         error_message = None
-        if (not customer.first_name):
+        if not customer.first_name:
             error_message = "Please Enter your First Name !!"
-        elif len (customer.first_name) < 3:
+        elif len(customer.first_name) < 3:
             error_message = 'First Name must be 3 char long or more'
         elif not customer.last_name:
             error_message = 'Please Enter your Last Name'
-        elif len (customer.last_name) < 3:
+        elif len(customer.last_name) < 3:
             error_message = 'Last Name must be 3 char long or more'
         elif not customer.phone:
             error_message = 'Enter your Phone Number'
-        elif len (customer.phone) < 10:
+        elif len(customer.phone) < 10:
             error_message = 'Phone Number must be 10 char Long'
-        elif len (customer.password) < 5:
+        elif len(customer.password) < 5:
             error_message = 'Password must be 5 char long'
-        elif len (customer.email) < 5:
+        elif len(customer.email) < 5:
             error_message = 'Email must be 5 char long'
-        elif customer.isExists ():
+        elif customer.isExists():
             error_message = 'Email Address Already Registered..'
-        # saving
-
         return error_message
